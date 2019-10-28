49bdc25604d0efb175ebd99d0321e3933fc3c441
Sydney Hunter <sydney.jane.hunter@gmail.com>
2019-10-28 14:16:56 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -20,7 +20,7 @@ def hopper
 end
 
 def alan_kay_is_known_for
-	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
+	
 	
 	programmer_hash = 
  		{
@@ -58,11 +58,6 @@ def dennis_ritchies_language
 end
 
 def adding_matz
-# add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#    :languages => ["LISP", "C"]
-# }
 
 	programmer_hash = 
  		{
